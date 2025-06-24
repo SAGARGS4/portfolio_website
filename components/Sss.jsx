5906cf294b3938e620e8a22c627fbf62af39f8e3
SAGARGS4 <sags22is@cmrit.ac.in>
2025-06-24 22:55:38 +0530

    components  updated

--- components/Sss.jsx
+++ components/Sss.jsx
@@ -6,3 +6,27 @@ function fjs() {
 		</div>
 	);
 }
+const Skill = () => {
+	return (
+		<div className="text-2xl font-semibold mb-4">
+			{Object.entries(skills).map(([section, items]) => (
+				<section key={section} className="mb-10">
+					<h2 className="text-2xl font-semibold mb-4">{section}</h2>
+					<div className="grid grid-cols-5 ">
+						{items.map((skill, index) => (
+							<div
+								key={index}
+								className="flex flex-col items-center text-center"
+							>
+								<div className="bg-gray-100 rounded-full w-16 h-16 flex items-center justify-center">
+									<img src={skill.icon} alt={skill.name} className="w-8 h-8" />
+								</div>
+								<span className="mt-2 text-sm">{skill.name}</span>
+							</div>
+						))}
+					</div>
+				</section>
+			))}
+		</div>
+	);
+};
